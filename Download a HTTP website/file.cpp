a11f04ecd1975cf0002367c7d97c7223c115b58b
abhaykrishna2003 <96983680+abhaykrishna2003@users.noreply.github.com>
2023-08-23 20:40:37 +0530

    Update and rename Download_HTTP_File.cpp to Download a HTTP website/file.cpp

--- Download a HTTP website/file.cpp
+++ Download a HTTP website/file.cpp
@@ -10,22 +10,27 @@
 #include <fstream>
 
 using namespace std;
-#define PORT 80
+
 int main()
 {
 	char dom[100];
+	
+	//asking user for a domain name
 	cout << "Enter domain name: ";
 	cin >> dom;
+
+	//getting the IP address from the domain name
 	struct hostent *ghbn;
 	ghbn = gethostbyname(dom);
-	char* s1 = inet_ntoa(*((struct in_addr *)(ghbn->h_addr)));
-	cout << "IP address of " << dom << " is " << s1 << endl;
+	char* IP = inet_ntoa(*((struct in_addr *)(ghbn->h_addr)));
+	cout << "IP address of " << dom << " is " << IP << endl;
 	
 	
 	
-	const char *server_ip = s1; 
+	const char *server_ip = IP; 
 	const int server_port = 80;      
 
+	//create a socket
 	int client_socket = socket(AF_INET, SOCK_STREAM, 0);
 	if (client_socket == -1) {
 		perror("Error creating socket");
@@ -41,11 +46,14 @@ int main()
 		return 1;
 	}
 
+	//connecting to the socket
 	if (connect(client_socket, (struct sockaddr *)&server_address, sizeof(server_address)) == -1) {
 		perror("Error connecting to server");
 		return 1;
 	}
 
+
+	//sending message to connected server
 	char message[256000];
 	sprintf(message, "GET / HTTP/1.0\r\n\r\n");
 
@@ -56,24 +64,35 @@ int main()
 	printf("Message sent to server: %s\n", message);
 	
 	char recieve[2560];
-	
+
+	//recieving the message from server
 	string str;
 	printf("Message recieved from server: \n");
-	
+
+	//since we dont know the length of the HTML file we grab the file by parts
+	// and append it into the string str.
 	while(recv(client_socket,recieve,sizeof(recieve),0)>0)
 	{
 		str = str + recieve;
 		memset(recieve,0,sizeof(recieve));
 	}
-	cout << str;
+
+	//closing connection.
 	close(client_socket);	
 	
-	
+
+	//Entering name of file to be saved in the system.
 	string filename;	
 	cout << endl << "Enter Filename: ";
 	cin >> filename;
+
+	//saving the file as .html file
 	string name = filename + ".html";
+
+	//creating a file
 	ofstream inputfile(name);
+	
+	//writing into the file
 	inputfile << str;	
 	
 	return 0;
